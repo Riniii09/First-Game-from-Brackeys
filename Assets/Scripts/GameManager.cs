f29e7dac0993b263c9377080617f0a83fc3048f2
Riniii09 <rinippillai@gmail.com>
2022-11-12 16:45:22 +0530

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool GameHasEnded = false; 
+    public float RestartDelay = 1f;
+    public GameObject LevelCompleteUI;
+    
+    public void LevelCompleted()
+    {
+        LevelCompleteUI.SetActive(true);
+    }
+    public void EndGame()
+    {
+        if(GameHasEnded==false)
+        {
+            GameHasEnded=true;
+            Debug.Log("Game Over");
+            Invoke("Restart", RestartDelay);
+        }
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float ForwardForce=100f;
+    public float SidewaysForce=100f;
+    public float JumpHeight=10000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    //Yaha pe FixedUpdate isliye use kiya kyuki UNITY LIKES THAT WHEN WE ARE MESSING WITH UNITY 
+    //PHYSICS ehehehheheehe thats it lol :P
+    void FixedUpdate() 
+    
+    {
+        //yeh time wala thoda jhol hai, baadme samjhungi
+            rb.AddForce(0, 0, ForwardForce*Time.deltaTime);
+            
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(SidewaysForce*Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if(Input.GetKey("a"))
+        {
+            rb.AddForce(-SidewaysForce*Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if(rb.position.y < 0f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
